b03a0e20fd3916e2e60eafd729ce9de5c0333c0a
Hunteryer <850491358@qq.com>
2020-04-22 21:19:46 +0800

    222

--- JavaEE-cc/src/main/java/aspect/Aspect.java
+++ JavaEE-cc/src/main/java/aspect/Aspect.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 
-@Aspect
 @Component
 public class Aspect {
 
-    @Pointcut("execution(* org.example.spring.mvc.service.*.*(..))")
+    @Pointcut("execution(* jdbc.*.*(..))")
     public void service(){}
 
 
@@ -22,18 +21,12 @@ public class Aspect {
         System.out.println("执行的方法：" + joinPoint.getSignature());
     }
 
-
     @After("service()")
     public void afterService(){
         System.out.print(new Date()+":");
         System.out.println("方法执行完毕！");
         System.out.println("----------------after----------------");
     }
-
-//    @Around("service()")
-//    public void aroundService(ProceedingJoinPoint joinPoint){
-//
-//    }
 
 
 }
